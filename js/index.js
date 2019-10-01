81746180afdd0ab22b13d82ef82a9af491142edf
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-10-01 16:45:35 -0400

    bus drives off page animation

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// DARK AND LIGHT MODE TOGGLE
+//---------------- START DARK AND LIGHT MODE TOGGLE SECTION ----------------//
 
 // create navBar variable to append buttons to
 const navBar = document.querySelector('nav');
@@ -18,14 +18,17 @@ lightModeBtn.id = "lightModeBtn";
 navBar.append(lightModeBtn);
 lightModeBtn.setAttribute("style", "display: none; color: yellow; background: black; border: 1px solid yellow;");
 
-// node list for tags and classes targeted but light mode and dark mode, to be used in event listeners below
-let tags = document.querySelectorAll(".btn, a, h1, h2, h4, p");
+// node list for tags and classes targeted by light mode and dark mode, to be used in event listeners below
+const tags = document.querySelectorAll(".btn, a, h1, h2, h4, p");
+
+// variable for body tag, used in dark/light mode and wheel scroll animation
+const bodyTag = document.getElementsByTagName("body");
 
 // event listener for dark mode on click of Mode
 darkModeBtn.addEventListener("click", darkMode);
 function darkMode () {
   // change backgrounds to black
-  document.getElementsByTagName("body")[0].style.backgroundColor = 'black';
+  bodyTag[0].style.backgroundColor = 'black';
   document.getElementsByTagName("header")[0].style.backgroundColor = 'black';
   document.getElementsByTagName("footer")[0].style.backgroundColor = 'black';
   tags.forEach(tag => {
@@ -50,5 +53,24 @@ function lightMode () {
   document.getElementById("darkModeBtn").style.display = "inline";
   document.getElementById("lightModeBtn").style.display = "none";
 };
+
+//---------------- END DARK AND LIGHT MODE TOGGLE SECTION ----------------//
+
+
+
+
+//-------------------- START WHEEL SCROLL ANIMATION ----------------------//
+
+// grab bus logo
+const busLogo = document.querySelector(".home img");
+
+// add event listener to make bus drive off page
+busLogo.addEventListener("wheel", () => {
+    busLogo.setAttribute("style", "transform: translateX(2000px); transition: all 2s;");
+});
+
+//-------------------- END WHEEL SCROLL ANIMATION ----------------------//
+
+
 
 
